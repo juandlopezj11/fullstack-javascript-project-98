06b419627c425ef412a9d829f353ef449ad712ae
juandlopezj11 <juan.lopez0611@gmail.com>
2025-01-29 18:09:44 -0500

    bin/brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 
-import readlineSync from 'redline-sync';
+import readlineSync from 'readline-sync';
 
 console.log('node bin/brain-games.js');
 console.log('¡Bienvenido a Brain Games!');
 
-const name = readlineSync.question('¿Cuál es tu nombre? ');
-console.log(`¡Hola, ${name}!`);
